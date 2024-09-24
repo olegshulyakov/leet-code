baa76baece53152155b70572a3026c2e27d15f0c
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2024-09-24 14:39:36 +0300

    add template

---
+++ .template.js
@@ -0,0 +1,18 @@
+/*
+
+Description
+
+*/
+
+/**
+ *
+ * @param {string} s
+ * @returns {string}
+ */
+function func(s) {
+    return s;
+}
+
+describe('', () => {
+    test('', () => expect(func("")).toEqual(""));
+})
