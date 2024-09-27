7971edecb161665ff2d39a3399f3c995594449f4
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2024-09-27 14:18:21 +0300

    1679. Max Number of K-Sum Pairs

--- LeetCode 75/Two Pointers/1679. Max Number of K-Sum Pairs.test.js
+++ LeetCode 75/Two Pointers/1679. Max Number of K-Sum Pairs.test.js
@@ -42,19 +42,28 @@ var maxOperations = function (nums, k) {
     if (nums.length < 2) return 0;
 
     let ops = 0;
-    for (let i = 0; i < nums.length - 1; i++) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
         if (nums[i] > k) {
             continue;
         }
 
-        const temp = k - nums[i];
-        for (let j = nums.length - 1; j > i; j--) {
-            if (nums[j] === temp) {
-                nums.splice(j, 1);
-                nums.splice(i, 1);
-                i--;
-                ops++;
-                break;
+        const el = nums[i];
+        const difference = k - el;
+        if (map.has(difference)) {
+            let counter = map.get(difference);
+            if (counter > 1) {
+                map.set(difference, --counter);
+            } else {
+                map.delete(difference);
+            }
+            ops++;
+        } else {
+            if (map.has(el)) {
+                let counter = map.get(el);
+                map.set(el, ++counter);
+            } else {
+                map.set(el, 1);
             }
         }
     }
@@ -65,5 +74,6 @@ var maxOperations = function (nums, k) {
 describe('1679. Max Number of K-Sum Pairs', () => {
     test('[1, 2, 3, 4], 5', () => expect(maxOperations([1, 2, 3, 4], 5)).toEqual(2));
     test('[3, 1, 3, 4, 3], 6', () => expect(maxOperations([3, 1, 3, 4, 3], 6)).toEqual(1));
+    test('[[2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2], 3', () => expect(maxOperations([2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2], 3)).toEqual(4));
     test('[5000000 .. 5000000], 10000000', () => expect(maxOperations(new Array(10000).fill(5000000), 10000000)).toEqual(5000));
 })
