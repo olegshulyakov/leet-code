4881c7b4b848e50a04ab2f5438e28072cdbd5859
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2024-10-10 14:38:09 +0300

    206. Reverse Linked List

---
+++ LeetCode 75/Linked List/206. Reverse Linked List.test.js
@@ -0,0 +1,69 @@
+/*
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+
+Example 3:
+
+Input: head = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+*/
+
+const ListNode = require('./ListNode');
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (head == null || head.next == null) {
+        return head;
+    }
+
+    let current = new ListNode(head.val, head.next),
+        previous = null,
+        temp = null;
+    while (current != null) {
+        temp = current.next;
+        current.next = previous;
+        previous = current;
+        if (temp == null) {
+            head = current;
+            current = null;
+        } else {
+            current = temp;
+        }
+    }
+    return head;
+};
+
+describe('206. Reverse Linked List', () => {
+    test.each([
+        { nums: [1, 2, 3, 4, 5], expected: [5, 4, 3, 2, 1] },
+        { nums: [1, 2], expected: [2, 1] },
+        { nums: [], expected: [] },
+        { nums: [1], expected: [1] },
+    ])('$nums', ({ nums, expected }) => expect(ListNode.toArray(reverseList(ListNode.from(nums)))).toEqual(expected));
+})
